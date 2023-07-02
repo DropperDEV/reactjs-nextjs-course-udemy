e1b073598615da3d546656167bd04b234cfe0d4a
James Costa <jamescosta4747@Gmail.com>
2023-07-02 14:55:47 -0300

    Start course udemy react

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component {
+  state = {
+      name: 'James Costa',
+      counter: 0
+    };
+  handleClick = () => {
+    this.setState({ name: 'James' })
+  }
+
+  HandCounter = (event) => {
+    event.preventDefault();
+    const { counter } = this.state;
+    this.setState({ counter: counter +1 })
+  }
+
+  render() {
+    const { name, counter } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handleClick}>
+            {name} {counter}
+          </p>
+          <a href='https://www.peppapig.com/pt-br' onClick={this.HandCounter}>
+            Hellow
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default App;
